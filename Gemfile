9bd6489d77047881a54489c1375b2143a1324cb5
Ali Quintanilha <alipq7@gmail.com>
2022-12-29 22:17:02 -0300

    Include input parser tests

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.1.3'
 
 gem 'json'
+gem 'rspec'
--- spec/input_parser_spec.rb
+++ spec/input_parser_spec.rb
@@ -3,11 +3,17 @@ require_relative '../helpers/input_parser'
 require_relative '../model/line_item'
 
 describe InputParser do
-  context “When testing the InputParser class” do
-    let (:line_items) { [LineItem.new(3, "book at", 6.00)] }
-    it "parses input" do
-      input_parser = InputParser.new
-      expect(input_parser.parse('test_input')).to eq(:line_items)
+  subject(:instance) { Class.new.include(InputParser).new }
+  let (:line_items) { [LineItem.new(3, "book at", 6.0)] }
+  let (:scanned_line) { ["2", "books", "at", "10.0"] }
+  let (:output_description) { "book at" }
+
+  context "When parsing input" do
+    it "singularizes descriptions" do
+      expect(subject.singularized_description(scanned_line, 1..scanned_line.length - 2)).to eq(output_description)
+    end
+    it "returns parsed input" do
+      expect(subject.parse('spec/input_with_equiv_description')).to eq(line_items)
     end
   end
 end
